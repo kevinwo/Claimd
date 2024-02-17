3dc6f6c83879a4f66234b86110e0098b6f121741
Kevin Wolkober <kevin.wolkober@gmail.com>
2024-02-17 14:30:23 -0600

    Get app clip up and running

--- App.tsx
+++ App.tsx
@@ -2,10 +2,18 @@ import "@thirdweb-dev/react-native-compat";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  isClip,
+  displayOverlay,
+  setSharedCredential,
+  getSharedCredential,
+} from "react-native-app-clip";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {isClip() && <Text>App is running as a clip</Text>}
       <StatusBar style="auto" />
     </View>
   );
